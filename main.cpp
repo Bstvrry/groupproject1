471da2c03b2dc5bcb34881c3e5cf83cb70139aaf
Nicholas njue <167431734+nicknjue@users.noreply.github.com>
2024-07-11 15:05:19 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,52 +1,25 @@
 
-// Function to add a new student
-void addStudent(vector<Student>& students, vector<Club>& clubs, vector<Sport>& sports) {
-    Student newStudent;
-    cout << "\nEnter Student First name: ";
-    cin >> newStudent.firstname;
-    cout << "Enter Student Last Name: ";
-    cin >> newStudent.sirname;
-    cout << "Enter Student Gender (M/F): ";
-    cin >> newStudent.Gender;
-    cout << "Enter Student Age: ";
-    cin >> newStudent.Age;
-    cout << "Enter Student Group (1-3): ";
-    cin >> newStudent.Group;
-
-    cout << "Choose activities (enter the names separated by space, type 'end' to finish): ";
-    string activity;
-    while (cin >> activity && activity != "end") {
-        newStudent.activities.push_back(activity);
+// Function to display all students
+void viewStudents(const vector<Student>& students) {
+    if (students.empty()) {
+        cout << "\nNo students added yet.\n";
+        return;
     }
-
-    for (const auto& activity : newStudent.activities) {
-        bool found = false;
-        for (auto& club : clubs) {
-            if (club.name == activity) {
-                if (newStudent.Gender == 'M') club.maleCount++;
-                else if (newStudent.Gender == 'F') club.femaleCount++;
-                found = true;
-                break;
+    cout << "\nStudent List:\n";
+    for (const auto& student : students) {
+        cout << "Name: " << student.firstname << " " << student.sirname << endl;
+        cout << "Gender: " << student.Gender << endl;
+        cout << "Age: " << student.Age << endl;
+        cout << "Group: " << student.Group << endl;
+        cout << "Activities: ";
+        if (student.activities.empty()) {
+            cout << "None\n";
+        } else {
+            for (const auto& activity : student.activities) {
+                cout << activity << " ";
             }
+            cout << endl;
         }
-        if (!found) {
-            for (auto& sport : sports) {
-                if (sport.name == activity) {
-                    if (newStudent.Gender == 'M') sport.maleCount++;
-                    else if (newStudent.Gender == 'F') sport.femaleCount++;
-                    break;
-                }
-            }
-        }
+        cout << "---------------------\n";
     }
-
-    students.push_back(newStudent);
-    cout << "\nStudent added successfully!\n";
-}
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
 }
